0c5a71673fb5ea03f7ec0eba1e3c7eda0496b4e0
Massaki <marcio.massaki@gmail.com>
2015-09-15 11:01:10 -0300

    Setting Computer Name and Disabling Handoff

--- scripts/osx-prep.sh
+++ scripts/osx-prep.sh
@@ -19,3 +19,28 @@ if ! xcode-select -p >/dev/null 2>&1; then
 else
   echo -e "\033[32mCommand line tools already installed. \033[0m"
 fi
+
+# Set Computer Name
+echo -en "\033[33m"
+echo "------------------------------"
+echo "Setting Computer Name..."
+echo -en "\033[0m"
+read -p "Computer Name: " COMPUTER_NAME
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+unset COMPUTER_NAME
+
+
+# Disable Handoff
+# From: https://jamfnation.jamfsoftware.com/discussion.html?id=12545
+echo -en "\033[33m"
+echo "------------------------------"
+echo "Disabling Handoff..."
+echo -en "\033[0m"
+UUID=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Hardware UUID" | cut -c22-57)
+PREF_FILE="$HOME/Library/Preferences/ByHost/com.apple.coreservices.lsuseractivityd.${UUID}.plist"
+
+defaults write $PREF_FILE ActivityAdvertisingAllowed -bool NO
+defaults write $PREF_FILE ActivityReceivingAllowed -bool NO
+
+unset UUID
+unset PREF_FILE
